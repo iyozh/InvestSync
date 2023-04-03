e33926b8452c141603833271d8fa89d5ed3df402
Ilya Yeuzhanka <evzhenko1106@gmail.com>
2023-04-03 17:18:17 +0300

    [IS-2] code clean

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -6,26 +6,12 @@ from sqlalchemy import pool
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def get_url():
     user = os.getenv("MYSQL_USER", "user")
@@ -69,7 +55,6 @@ def run_migrations_online() -> None:
 
     configuration = config.get_section(config.config_ini_section)
     configuration["sqlalchemy.url"] = get_url()
-    print(configuration)
     connectable = engine_from_config(
         configuration, prefix="sqlalchemy.", poolclass=pool.NullPool,
     )
