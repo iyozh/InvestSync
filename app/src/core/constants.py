e5ba87de8bec61dbb97b51394c65459a460f0671
Ilya Yeuzhanka <evzhenko1106@gmail.com>
2023-04-06 00:24:39 +0300

    [IS-5] project structure refactoring

--- app/src/core/constants.py
+++ app/src/core/constants.py
@@ -1,1 +1,3 @@
 DEFAULT_FIELD_PLACEHOLDER = '—'
+INITIAL_TICKERS = ['AAPL', 'TSLA', 'MSFT', 'META','AMZN']
+COMPANY_OVERVIEW_API_URL = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol={symbol}&apikey={api_key}'
--- app/src/models/ticker.py
+++ app/src/models/ticker.py
@@ -1,8 +1,16 @@
 import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+
 from app.src.db.base_class import Base
+from app.src.models.ticker_overview import TickerOverview
 
 
 class Ticker(Base):
     __tablename__ = 'ticker'
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     symbol = sa.Column(sa.VARCHAR(length=10))
+
+    ticker_overview = relationship(TickerOverview, uselist=False)
+
+    def __init__(self, symbol: str):
+        self.symbol = symbol
