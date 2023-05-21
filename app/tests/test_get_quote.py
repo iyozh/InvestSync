3c58671495ca44c67cbeb70b61f86d1107ec3b77
Ilya Yeuzhanka <evzhenko1106@gmail.com>
2023-05-21 19:33:14 +0300

    [IS-19] aioredis with rate limit

--- app/tests/test_get_quote.py
+++ app/tests/test_get_quote.py
@@ -4,18 +4,18 @@ from unittest.mock import patch
 from fastapi.testclient import TestClient
 from app.src.main import app
 
-client = TestClient(app)
 
 @patch("app.src.services.redis_service.RedisService.get_cached_value")
 @patch("httpx.get")
 def test_get_quote(mock_httpx_get, mock_get_cached_value):
-    with open(f"{Path().absolute()}/test_data/quote.json") as fp:
-        mocked_quote_response = json.loads(fp.read())
+    with TestClient(app) as client:
+        with open(f"{Path().absolute()}/test_data/quote.json") as fp:
+            mocked_quote_response = json.loads(fp.read())
 
-    mock_get_cached_value.return_value = mocked_quote_response
-    mock_httpx_get.return_value.json.return_value = mocked_quote_response
+        mock_get_cached_value.return_value = mocked_quote_response
+        mock_httpx_get.return_value.json.return_value = mocked_quote_response
 
-    response = client.get("/tickers/quote/META")
+        response = client.get("/tickers/quote/META")
 
-    assert response.status_code == 200
-    assert response.json() == mocked_quote_response
+        assert response.status_code == 200
+        assert response.json() == mocked_quote_response
